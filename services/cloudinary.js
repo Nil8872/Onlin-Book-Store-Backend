3b541068627e136da479ff3fbcc6976a548af85b
Nilesh <sariyanilesh7265@gmail.com>
2023-08-16 20:52:56 +0530

    Url change

--- services/cloudinary.js
+++ services/cloudinary.js
@@ -13,7 +13,8 @@ console.log(process.env.CLOUDINARY_API_KEY);
 
 const uploadBase64Image = (base64)=>{
     return new Promise((resolve, reject)=>{
-        cloudinary.v2.uploader.upload(base64, { resource_type: 'image', public_id: "olympic_flag" }, (error, result)=>{
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cloudinary.v2.uploader.upload(base64, { resource_type: 'image', public_id: uniqueSuffix }, (error, result)=>{
             if(error){
 
                 console.log(error)
