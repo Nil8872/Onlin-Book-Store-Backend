e5bf081a37c2dc52a998d6264d0f869a23cea0c3
Nilesh <sariyanilesh7265@gmail.com>
2023-08-16 18:21:29 +0530

    Limit set for max data recieved

--- index.js
+++ index.js
@@ -71,7 +71,8 @@
     app.use("/api/category", category);
     app.use("/api/cart", cart);
 
-
+    app.use(express.json({ limit: '50mb' }));
+    app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
     app.listen( process.env.PORT || PORT, () => {
         console.log(`listening on port ${PORT}`);
